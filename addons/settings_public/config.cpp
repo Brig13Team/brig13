92d7d08ed62a46f2329a08589c3a55e377607e6e
Thomas Müller <Dorbedo@t-online.de>
2018-01-15 17:07:38 +0100

    settings splitted

---
+++ addons/settings_public/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+    class cba_settings_userconfig {
+        author = "$Dorbedo";
+        name = "Brigade 13 Settings";
+        url = "https://github.com/Brig13Team/brig_settings";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"cba_settings"};
+        version = 1.0;
+        authors[] = {"Dorbedo"};
+    };
+};
